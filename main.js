e578b11f3d0d9e46e953d1c0f14b8dc8e9566999
hamsadhanabalan <hamsadhanabalan@gmail.com>
2026-02-17 11:23:44 +0530

    route structure

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const PORT=6969;
 app.get("/",(req,res)=>{
   res.json({msg:"Hello students"});
   });
+
+  app.get("/movies",()=>{
+
+  })
+  app.post("/movies",()=>{
+    
+  })
+  app.put("/movies/:id",()=>{
+    
+  })
+  app.delete("/movies/:id",()=>{
+    
+  })
   app.listen(PORT, ()=>{
     console.log(`The server is running at http://localhost:${PORT}`
     );
